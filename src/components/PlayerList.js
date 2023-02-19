846554344933c30b64776ae670a354e7b34a593a
Jaewon Bae <jaey.pro@gmail.com>
2023-02-19 01:19:00 -0500

    added helpers for player data and adding player wins

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,6 +1,13 @@
 import Player from "./Player";
+import matchData from "../data/matchData";
+import playerData from "../data/playerData";
+import { preparePlayerData, addWinsToPlayers } from "../helpers/playerHelpers";
 
 function PlayerList() {
+  const playerDataArray = preparePlayerData(playerData);
+  const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
+  console.log(playerDataArray);
+  console.log(parsedPlayerData);
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
